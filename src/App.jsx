18702a6ad9d411ba7a288c84e46c13a33bcd5104
olayode demola <demolaolayode6@gmail.com>
2026-09-12 18:35:11 +0100

    feat: add browser routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+
 function App() {
-  return <div>FinQuard</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about-us" element={<AboutUs />} />
+        <Route path="contact-us" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
