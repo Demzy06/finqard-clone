c3a21c0d497f519685b50177ba579519e7b08523
olayode demola <demolaolayode6@gmail.com>
2026-09-18 00:23:40 +0100

    feat: create Homepage  component + setup ui layout

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,22 @@
+import Header from "../../components/Header";
+import HighlightsPrev from "../../components/HighlightsPrev";
+import Testimonials from "../../components/Testimonials";
+import TickerSection from "../../components/TickerSection";
+import HomePageHeroSection from "./component/HomePageHeroSection";
+
+function HomePage() {
+  return (
+    <div className="h-fit">
+      <Header />
+
+      <main className=" bg-white pb-10 h-fit">
+        <HomePageHeroSection />
+        <TickerSection />
+        <HighlightsPrev />
+        <Testimonials />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
