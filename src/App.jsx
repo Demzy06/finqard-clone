86bdb55928971ac749a02d464752b7a10eeee3c2
olayode demola <demolaolayode6@gmail.com>
2026-09-18 00:25:35 +0100

    feat: re-import Homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
+import HomePage from "./pages/homepage/HomePage";
 
 function App() {
   return (
