b2b26a1ed48c50a85737b8ed4cff22c1010bc38f
olayode demola <demolaolayode6@gmail.com>
2026-09-18 01:15:31 +0100

    feat: add navLinks data

---
+++ src/data/Navbar.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    title: "Home",
+    path: "/"
+  },
+  {
+    title: "Products",
+    path: "/"
+  },
+  {
+    title: "About Us",
+    path: "about-us"
+  },
+  {
+    title: "FAQs",
+    path: "/"
+  },
+  {
+    title: "Support",
+    path: "contact"
+  },
+]
