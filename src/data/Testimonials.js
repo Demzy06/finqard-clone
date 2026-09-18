5514df3601dc237cf5ba886e845029e0a6957cff
olayode demola <demolaolayode6@gmail.com>
2026-09-18 01:05:38 +0100

    feat: add testimonials data array

---
+++ src/data/Testimonials.js
@@ -0,0 +1,46 @@
+import Avatar1 from "../assets/avatars/avatar1.webp";
+import Avatar2 from "../assets/avatars/avatar2.webp";
+import Avatar3 from "../assets/avatars/avatar3.webp";
+import Avatar4 from "../assets/avatars/avatar4.webp";
+import Avatar5 from "../assets/avatars/avatar5.webp";
+
+export const briefSectionHighlight = {
+  title: "Testimonial",
+  headerText: "Here's what our users are chatting about!",
+  paragraphText: "Our financial management platform is transforming money management. Here's what users say."
+}
+
+export const testimonials = [
+  {
+    name: "Joseph Ogugua",
+    username: "@mubaarakadul",
+    avatar: Avatar1,
+    paragraphOne: "The app is incredibly easy to use, transactions are fast, and the rates are great. I also love the transparency—you always know exactly what to expect before confirming a trade.",
+    paragraphTwo: "I'll gladly recommend it to whoever is looking for a secure platform to trade and make transactions!",
+    type: 'base'
+  },
+  {
+    name: "Mubaarak Abdulhameed",
+    username: "@jogugua",
+    avatar: Avatar2,
+    paragraphOne: "I have used a number of other gift card apps to trade my gift cards for cash and got ripped off a number of times and then I went on Google and searched again then I found FinQard. To be honest since I have been trading with them I haven't had any bitter experience up till date.",
+  },
+  {
+    name: "Bayo Faleye",
+    username: "@bayofaleye",
+    avatar: Avatar3,
+    paragraphOne: "The app is incredibly easy to use, transactions are fast, and the rates are great. I also love the transparency—you always know exactly what to expect before confirming a trade.",
+  },
+  {
+    name: "Ezinne Elechi",
+    username: "@ezinneelechi",
+    avatar: Avatar4,
+    paragraphOne: "When I needed cash for a gift card, FinQard made the process incredibly smooth. From verification to withdrawal, everything was fast, simple, and seamless!",
+  },
+  {
+    name: "Susan Duncan",
+    username: "@susanduncan",
+    avatar: Avatar5,
+    paragraphOne: "I've had a great experience using FinQard so far. The app is clean and easy to navigate, making it simple to buy and sell gift cards, get virtual USD cards, and pay bills without stress. Transactions are fast, reliable, and convenient.",
+  },
+]
