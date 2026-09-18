ce3ae869d7c888f6d6564bb9a16de8880c3109c7
olayode demola <demolaolayode6@gmail.com>
2026-09-18 01:19:48 +0100

    feat: setup code structure + styling

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,23 @@
+import BriefHighlight from "./BriefSectionHighlight";
+import { briefSectionHighlight, testimonials } from "../data/Testimonials";
+import TestimonialCard from "./TestimonialCard";
+
 function Testimonials() {
-  return <div>Testimonials</div>;
+  return (
+    <section className="pt-15 pb-10 ">
+      <BriefHighlight
+        title={briefSectionHighlight.title}
+        headerText={briefSectionHighlight.headerText}
+        paragraphText={briefSectionHighlight.paragraphText}
+      />
+
+      <div className="px-4">
+        {testimonials.map((testimony) => (
+          <TestimonialCard testimony={testimony} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials;
