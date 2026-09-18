b63e5feaf0fd1629f4c933774ad0e9583b681aa6
olayode demola <demolaolayode6@gmail.com>
2026-09-18 01:15:52 +0100

    feat: add features data array

---
+++ src/data/HighlightPrev.js
@@ -0,0 +1,61 @@
+import AmazonImg from "../assets/secondary/amazon-img.webp";
+import CardCoinImg from "../assets/secondary/card-coin-img.webp"
+import VisaCardImg from "../assets/secondary/visa-card.webp"
+import TurboImg from "../assets/secondary/turbo-img.webp"
+import ReceiptImg from "../assets/secondary/receipt-img.webp"
+
+
+export const features = [
+  {
+    title: "Receive your payments immediately, no delays!",
+    paragraph:
+      "Manage your cards effortlessly and keep track of all of your expensense",
+    image: AmazonImg,
+    bodyStyle: "bg-purple-700 pt-10 pl-5 rounded-2xl h-fit pb-0.5",
+    titleStyle: "pb-4 text-white text-[30px] font-[430]",
+    paragraphStyle: "text-white text-[18px] font-light w-[75%] tracking-wide leading-5.5"
+  },
+  {
+    title: "Exchange Giftcards to Crypto",
+    paragraph:
+      "Turn your giftcards into crypto instantly and securely",
+    image: CardCoinImg,
+    bodyStyle: "bg-grey-100 pt-10 pl-5 rounded-2xl h-fit pb-0.5",
+    titleStyle: "pb-4 text-purple-700 text-[30px] font-[430]",
+    paragraphStyle: "text-purple-700 text-[18px] font-light w-[75%] tracking-wide leading-5.5"
+  },
+  {
+    title: "Easy easier card management",
+    paragraph:
+      "Manage your cards effortlessly and keep track all of your expensense",
+    image: VisaCardImg,
+    bodyStyle: "bg-white border border-grey-100 pt-10 pl-5 rounded-2xl h-fit pb-0.5",
+    titleStyle: "pb-4 text-black-700 text-[30px] font-[430]",
+    paragraphStyle: "text-grey-600 text-[18px] font-light w-[75%] tracking-wide leading-5.5"
+  },
+  {
+    title: "Instant deposit & withdrawals",
+    paragraph:
+      "Deposit and withdraw in less than 10 minutes",
+    image: TurboImg,
+    bodyStyle: "bg-black-700 border border-grey-100 pt-10 pl-5 rounded-2xl h-fit pb-0.5",
+    titleStyle: "pb-4 text-white text-[30px] font-[430]",
+    paragraphStyle: "text-white text-[18px] font-light w-[75%] tracking-wide leading-5.5"
+  },
+  {
+    title: "Pay Bills",
+    paragraph:
+      "Settle your electricity, cable TV, internet, airtime, and other utility bills quickly and securely—all in one place. No queues, no delays.",
+    image: ReceiptImg,
+    bodyStyle: "bg-grey-400 border border-grey-100 pt-10 pl-5 rounded-2xl h-fit pb-0.5",
+    titleStyle: "pb-4 text-black-700 text-[30px] font-[430]",
+    paragraphStyle: "text-grey-600 text-[18px] font-light w-[75%] tracking-wide leading-5.5"
+  },
+];
+
+export const briefHighlight = {
+  title: "Key Features",
+  headerText: "Hey, take a look at our cool features!",
+  paragraphText: "Our financial management platform is transforming money management. Here's what users say."
+
+}
