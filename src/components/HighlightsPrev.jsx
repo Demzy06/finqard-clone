d5f4d282531a0d1bf4066a4251c3dd1ed65ae9cd
olayode demola <demolaolayode6@gmail.com>
2026-09-18 01:00:14 +0100

    feat: setup ui code layout + styling

--- src/components/HighlightsPrev.jsx
+++ src/components/HighlightsPrev.jsx
@@ -1,5 +1,22 @@
+import HighlightPreviewCard from "./HighlightPreviewCard";
+import { features, briefHighlight } from "../data/HighlightPrev";
+import BriefHighlight from "./BriefSectionHighlight";
+
 function HighlightsPrev() {
-  return <div>Highlight</div>;
+  return (
+    <section className="py-6 pb-4 bg-grey-300">
+      <BriefHighlight
+        title={briefHighlight.title}
+        headerText={briefHighlight.headerText}
+        paragraphText={briefHighlight.paragraphText}
+      />
+      <section className="card px-4  ">
+        {features.map((feature) => (
+          <HighlightPreviewCard feature={feature} />
+        ))}
+      </section>
+    </section>
+  );
 }
 
 export default HighlightsPrev;
