2d02d71bd2df5c7f89c776b159e9fd03f7e92b0f
olayode demola <demolaolayode6@gmail.com>
2026-09-21 18:33:15 +0100

    feat: create NewsMediaPrev component , iterated over the mediaImages array to display media logos

--- src/components/NewsMediaPrev.jsx
+++ src/components/NewsMediaPrev.jsx
@@ -1,5 +1,18 @@
+import TheGuardianImg from "../assets/news-media/the-guardian.webp";
+import PulseImg from "../assets/news-media/pulse.webp";
+import TechCabalImg from "../assets/news-media/techcabal.webp";
+import PremiumImg from "../assets/news-media/premium.webp";
+
+const mediaImages = [TheGuardianImg, PulseImg, PremiumImg, TechCabalImg];
+
 function NewsMediaPrev() {
-  return <div>Media</div>;
+  return (
+    <section className="mt-18 mb-15 flex justify-evenly flex-wrap px-6 gap-y-5 align-middle">
+      {mediaImages.map((img) => (
+        <img src={img} alt="news-media-logo" className="h-8" />
+      ))}
+    </section>
+  );
 }
 
 export default NewsMediaPrev;
