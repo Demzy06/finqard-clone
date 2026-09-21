f2014771eeb31fb33b86e5890add4fca192b4c96
olayode demola <demolaolayode6@gmail.com>
2026-09-21 19:51:31 +0100

    feat: add finqardStatement data file

---
+++ src/data/finQardStatements.js
@@ -0,0 +1,16 @@
+export const statements = [
+  {
+    title: "Our Mission",
+    statementParagraphOne:
+      "To make gift card trading, virtual cards, bill payments, and everyday digital transactions faster, safer, and more accessible.",
+    statementParagraphTwo:
+      "At FinQard, our mission is to give users a trusted platform where they can easily buy, sell, and manage gift cards, create virtual cards, pay bills, and complete digital transactions with speed, confidence, and transparency.",
+  },
+  {
+    title: "Our Vision",
+    statementParagraphOne:
+      "To become Africa's most trusted digital gift card, virtual card, and payment platform.",
+    statementParagraphTwo:
+      "We envision a future where anyone can convert value, make payments, shop globally, and access simple digital financial services instantly, without stress, delays, or uncertainty.",
+  },
+];
