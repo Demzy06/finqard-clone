82f3e9dc277392aa5795f490b45d959ea70de786
olayode demola <demolaolayode6@gmail.com>
2026-09-21 19:50:51 +0100

    feat: create faqs data file

---
+++ src/data/Faqs.js
@@ -0,0 +1,20 @@
+export const briefHighlight = {
+  title: "Testimonials",
+  headerText: "Frequently Asked Questions",
+  paragraphText: "FinQard App simplifies your finances with real-time expense tracking and budgeting, making it easier to save for vacations and manage your bills."
+}
+
+export const accordionData = [
+  {
+    question: "Which platforms are comaptible with FinQard App?",
+    answer: "FinQard App makes managing your finances effortless with live expense tracking and budgeting, helping you save for trips and stay on top of your bills!"
+  },
+  {
+    question: "Which platforms support FinQard app for financial transactions?",
+    answer: "FinQard App makes managing your finances effortless with live expense tracking and budgeting, helping you save for trips and stay on top of your bills!"
+  },
+  {
+    question: "Which platforms are comaptible with FinQard App?",
+    answer: "FinQard App makes managing your finances effortless with live expense tracking and budgeting, helping you save for trips and stay on top of your bills!"
+  },
+]
