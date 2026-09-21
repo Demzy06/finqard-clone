8dc0eb8bd3aa95be46639bcc477251308c2caaab
olayode demola <demolaolayode6@gmail.com>
2026-09-21 19:52:25 +0100

    feat: add footer nav links datas

--- src/data/Navbar.js
+++ src/data/Navbar.js
@@ -1,3 +1,5 @@
+import { title } from "motion/react-client"
+
 export const navLinks = [
   {
     title: "Home",
@@ -19,4 +21,30 @@ export const navLinks = [
     title: "Support",
     path: "contact"
   },
+]
+
+
+export const quickLinksFooterNavLinks = [
+  { title: "Features", path: "/" },
+  { title: "About Us", path: "/" },
+  { title: "Blog", path: "/" },
+  { title: "Contact Us", path: "/" },
+]
+
+export const featuresFooterNavLinks = [
+  { title: "Giftcards", path: "/" },
+  { title: "Virtual Card", path: "/" },
+  { title: "Bill Payments", path: "/" },
+  { title: "Wallet Top Up", path: "/" },
+]
+
+export const legalFooterNavLinks = [
+  { title: "Terms & Conditions", path: "/" },
+  { title: "AML policy", path: "/" },
+  { title: "Privacy Policy", path: "/" },
+]
+
+export const socialsFooterNavLinks = [
+  { title: "Instagram", path: "/" },
+  { title: "X", path: "/" },
 ]
