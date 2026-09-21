3ac9befc7b426f122bb0a90b23e381ac80e80b98
olayode demola <demolaolayode6@gmail.com>
2026-09-21 16:19:25 +0100

    feat: create ContentItem component + set up layout + styling

---
+++ src/components/ContentItem.jsx
@@ -0,0 +1,18 @@
+function ContentItem() {
+  return (
+    <div className="bg-[#FAFAFA] p-5 py-7 rounded-4xl border border-gray-100">
+      <h2 className="text-[22px] font-[550] text-purple-700 bg-grey-100 py-3 p-3.5 w-fit h-fit rounded-full">
+        01
+      </h2>
+      <div className="mt-10 w-[85%]">
+        <h2 className="text-[23px] font-semibold">Trust & Security</h2>
+        <p className="mt-5 text-gray-600">
+          With our top-notch payment methods, you'll get paid for your gift card
+          swap in just a few minutes!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ContentItem;
