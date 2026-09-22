8c455e993b035afe461c312b672475a21e5491fe
olayode demola <demolaolayode6@gmail.com>
2026-09-22 21:51:19 +0100

    feat: add logic to scroll to faqs section , using useLocation

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -4,14 +4,28 @@ import Header from "../../components/Header";
 import HighlightsPrev from "../../components/HighlightsPrev";
 import Testimonials from "../../components/Testimonials";
 import TickerSection from "../../components/TickerSection";
+
 import HomePageHeroSection from "./component/HomePageHeroSection";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 function HomePage() {
+  const location = useLocation();
+  console.log(location);
+
+  useEffect(() => {
+    if (location.hash === "#faqs") {
+      document.getElementById("faqs")?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [location]);
+
   return (
     <div className="h-fit">
       <Header />
 
-      <main className=" bg-white pb-10 h-fit">
+      <main className=" bg-white h-fit">
         <HomePageHeroSection />
         <TickerSection />
         <HighlightsPrev />
