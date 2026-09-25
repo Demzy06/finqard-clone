8dbd6fffe7b35d02681044c0debd29fe6d5a9f9a
olayode demola <demolaolayode6@gmail.com>
2026-09-25 20:02:17 +0100

    style: adjust padding in paragraph

--- src/components/BriefSectionHighlight.jsx
+++ src/components/BriefSectionHighlight.jsx
@@ -1,7 +1,7 @@
 function BriefSectionHighlight({ title, headerText, paragraphText }) {
   return (
     <div className="flex flex-col text-center w-[75%] m-auto">
-      <div className="mb-6 px-8 w-fit py-1 rounded-xl bg-white m-auto">
+      <div className="mb-6 w-fit rounded-xl m-auto">
         <p className="text-[16px] font-[350] text-purple-600 border px-5 py-0.75 rounded-4xl border-purple-200 bg-grey-100">
           {title}
         </p>
