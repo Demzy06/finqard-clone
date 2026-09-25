b79d3581eb99f007b94b8e61e3ea7a32ba60483a
olayode demola <demolaolayode6@gmail.com>
2026-09-25 01:12:50 +0100

    fetat: create offer object and passed it down as a prop to WhatWeOffer component

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -1,15 +1,26 @@
 import WhatWeOffer from "../../components/WhatWeOffer";
 import Header from "../../components/Header";
-import FinQardStatement from "../../components/FinQardStatements";
+import FinQardStatement from "./component/FinQardStatements";
 import OurValues from "./component/OurValues";
 import Faqs from "../../components/Faqs";
 import Footer from "../../components/Footer";
+// import BackgroundImg from "../../assets/hero/hero-section-bg-aesthetics.svg";
+import AboutUsImg from "../../assets/hero/about-us.webp";
+
+const offer = {
+  bgImg: "./assets/hero/hero-section-bg-aesthetics.svg",
+  headerText: " We're building the future of giftcard trading",
+  paragraphText:
+    "We're revolutionizing the world of virtual card payments and bill management. Our app equips users to enable them to manage their finances effortlessly, from handling payments and tracking gift card trading.",
+  heroImg: AboutUsImg,
+  type: "hero",
+};
 function AboutUs() {
   return (
     <>
       <Header />
       <main className="">
-        <WhatWeOffer />
+        <WhatWeOffer offer={offer} />
         <FinQardStatement />
         <OurValues />
         <Faqs />
