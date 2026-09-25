eebaaa0a48ffa6b39d952a9354b98e8f7a850b32
olayode demola <demolaolayode6@gmail.com>
2026-09-25 01:34:27 +0100

    style: change Header bg color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
     //   className={`md:h-fit flex justify-between items-center px-8 py-14 w-full h-18 fixed top-0 left-0 right-0 z-1000 md:pl-4 md:pr-10 md:bg-[rgb(249, 249, 249)]/70 backdrop-blur-lg ${navIsOpen ? "bg-white" : "bg-[#F9F9F9]/70 backdrop-blur-lg"} md:h-0 ${true ? "opacity-100 ease-in translate-y-0" : "opacity-0 -translate-y-10"} transition-all duration-300`}
     // >
     <header
-      className={`md:h-fit flex px-8 py-9 items-center ${navIsOpen ? "bg-red-400" : "bg-[#F9F9F9]/70 backdrop-blur-lg"}`}
+      className={`md:h-fit flex px-8 py-9 items-center bg-[#F9F9F9]/20 backdrop-blur-lg sticky top-0 left-0 right-0 z-100 `}
     >
       <Logo />
       <Navbar navIsOpen={navIsOpen} setNavIsOpen={setNavIsOpen} />
