cec07557a10d326fa9307cac16d16e4816c05e9b
olayode demola <demolaolayode6@gmail.com>
2026-09-25 01:45:47 +0100

    feat: change hard coded value to dynamic values

--- src/components/TickerSection.jsx
+++ src/components/TickerSection.jsx
@@ -6,7 +6,7 @@ function TickerSection({ text, styles }) {
       <h3
         className={`${styles} text-[30px] font-[450] tracking-tight leading-9 w-[80%] ml-7 mb-10`}
       >
-        Explore our app to discover a variety of gift cards you can trade!
+        {text}
       </h3>
 
       <Ticker start="0%" end="-50%" styles="mb-5" />
