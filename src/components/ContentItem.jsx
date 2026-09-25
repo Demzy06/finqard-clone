5a7f8ea22b0f3c9183aff8d09846c0d2cd7f2849
olayode demola <demolaolayode6@gmail.com>
2026-09-25 01:25:22 +0100

    feat: change hard codded values to dynamic values coming from props

--- src/components/ContentItem.jsx
+++ src/components/ContentItem.jsx
@@ -1,15 +1,14 @@
-function ContentItem() {
+function ContentItem({ content, numbering }) {
   return (
     <div className="bg-[#FAFAFA] p-5 py-7 rounded-4xl border border-gray-100">
-      <h2 className="text-[22px] font-[550] text-purple-700 bg-grey-100 py-3 p-3.5 w-fit h-fit rounded-full">
-        01
+      <h2 className="text-[22px] font-[550] text-purple-700 bg-grey-100 py-3 px-5 w-fit h-fit rounded-full">
+        {numbering + 1}
       </h2>
       <div className="mt-10 w-[85%]">
-        <h2 className="text-[23px] font-semibold">Trust & Security</h2>
-        <p className="mt-5 text-gray-600">
-          With our top-notch payment methods, you'll get paid for your gift card
-          swap in just a few minutes!
-        </p>
+        <h2 className="text-[23px] font-semibold">
+          {content.contentHeaderText}
+        </h2>
+        <p className="mt-5 text-gray-600">{content.contentParagraph}</p>
       </div>
     </div>
   );
